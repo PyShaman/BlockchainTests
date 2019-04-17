ad5d0b3eecea300883ba4e687fb57ef6eb659455
michal.bek <michal.bek@eqsg.com>
2019-04-17 23:10:05 +0200

    addding tests

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # BlockchainTests
+
+
+download and install node.js and npm from https://nodejs.org/en/#download for windows
+
+for linux: https://github.com/nodejs/help/wiki/Installation
+
+for MacOS: 
+
+After downloading and installing node.js and npm you need to:
+```
+$ npm install -g blockchain-wallet-service
+```
+and then start wallet service:
+```
+$ blockchain-wallet-service start --port 3000
+```
+Note that blockchain-wallet-service is designed to be run locally on the same machine as your application and therefore will only accept connections from localhost. If you modify this service to accept external connections, be sure to add the appropriate firewall rules to prevent unauthorized use.
+
+An API code is required for wallet creation and higher request limits. For basic usage, no API code is required.
+
+After this user can run tests
--- tests.py
+++ tests.py
@@ -1,5 +1,11 @@
 import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
+
+session = requests.Session()
+retry = Retry(connect=3, backoff_factor=0.5)
+adapter = HTTPAdapter(max_retries=retry)
 
 payload_user_1 = {'password': 'password_user_1',
                   'api_code': 'api_code_user_1',
@@ -18,11 +24,33 @@ payload_user_4 = {'password': 'password_user_4',
                   'label': 'Main Adress',
                   'email': 'user_4@mail.com'}
 
-user_1_data = requests.post('http://localhost:3000/api/v2/create', params=payload_user_1)
-user_2_data = requests.post('http://localhost:3000/api/v2/create', params=payload_user_2)
-user_3_data = requests.post('http://localhost:3000/api/v2/create', params=payload_user_3)
-user_4_data = requests.post('http://localhost:3000/api/v2/create', params=payload_user_4)
+"""
+CREATE NEW USERS
+"""
+try:
+    user_1_data = requests.post('http://localhost:3000/api/v2/create', params=payload_user_1)
+except requests.exceptions.SSLError:
+    print("Connection refused")
+try:
+    user_2_data = requests.post('http://localhost:3000/api/v2/create', params=payload_user_2)
+except requests.exceptions.SSLError:
+    print("Connection refused")
+try:
+    user_3_data = requests.post('http://localhost:3000/api/v2/create', params=payload_user_3)
+except requests.exceptions.SSLError:
+    print("Connection refused")
+try:
+    user_4_data = requests.post('http://localhost:3000/api/v2/create', params=payload_user_4)
+except requests.exceptions.SSLError:
+    print("Connection refused")
 
+# user_2_data = requests.post('http://localhost:3000/api/v2/create', params=payload_user_2)
+# user_3_data = requests.post('http://localhost:3000/api/v2/create', params=payload_user_3)
+# user_4_data = requests.post('http://localhost:3000/api/v2/create', params=payload_user_4)
+
+"""
+TEST FOR CHECKING IF USER IS CREATED SUCCESSFULLY
+"""
 # Test 1 - check if account is created - by status code
 print(user_1_data.status_code)
 assert user_1_data.status_code == 200
@@ -44,27 +72,34 @@ user_3_address = user_3_data.json().get('address')
 user_4_guid = user_4_data.json().get('guid')
 user_4_address = user_4_data.json().get('address')
 
-print('U1: ' + user_1_guid + ', ' + user_1_address)
-print('U2: ' + user_2_guid + ', ' + user_2_address)
-print('U3: ' + user_3_guid + ', ' + user_3_address)
-print('U4: ' + user_4_guid + ', ' + user_4_address)
+print('[U1] guid: ' + user_1_guid + ', address: ' + user_1_address)
+print('[U2] guid: ' + user_2_guid + ', address: ' + user_2_address)
+print('[U3] guid: ' + user_3_guid + ', address: ' + user_3_address)
+print('[U4] guid: ' + user_4_guid + ', address: ' + user_4_address)
 
-# Outgoing payments between two users
-payment_user_1 = requests.post('https://blockchain.info/merchant/' + user_1_guid + '/.')
-print(payment_user_1.status_code)
-print(payment_user_1.json())
+"""
+TEST FOR CHECKING BALANCE
+"""
+# Check balance
+payload_balance_1 = {'password': payload_user_1.get('password'), 'api_code': payload_user_1.get('api_code')}
+try:
+    account_balance_1 = requests.get('http://localhost:3000/merchant/' + user_1_guid + '/balance', params=payload_balance_1)
+except requests.exceptions.SSLError:
+    print("Connection refused!")
+print('[BALANCE] ' + account_balance_1.json().get('balance'))
+print(account_balance_1.status_code)
+
+
+"""
+TEST FOR MAKING OUTGOING PAYMENTS
+"""
+
+# print(payload_payment_1.get('message'))
+# print(payment_user_1.text)
+# print(payment_user_1.get('tx_hash'))
+# print(payment_user_1.get('notice'))
 # print('Status: ' + str(payment_user_1.status_code) + ', message: ' + payment_user_1.json())
 
-# for i in range(1, 4):
-#     user = 'user_' + str(i) + '_data'
-#     if user.status_code == 200:
-#         print(str('user_' + i + ' created successfully!'))
-#     else:
-#         print(str('user_' + i + ' creation failed!'))
-# print(p.status_code)
-# print(p.json())
-# guid_data = p.json().get('guid')
-# address_data = p.json().get('address')
-# print(guid_data)
-# print(address_data)
+
+#
 
