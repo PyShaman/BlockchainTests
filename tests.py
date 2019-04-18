9754eeda5c91bad281432e5d88ef020e8d8fc19e
michal.bek <michal.bek@eqsg.com>
2019-04-18 06:58:59 +0200

    dding tests

--- tests.py
+++ tests.py
@@ -1,11 +1,4 @@
 import requests
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
-
-
-session = requests.Session()
-retry = Retry(connect=3, backoff_factor=0.5)
-adapter = HTTPAdapter(max_retries=retry)
 
 payload_user_1 = {'password': 'password_user_1',
                   'api_code': 'api_code_user_1',
@@ -81,25 +74,66 @@ print('[U4] guid: ' + user_4_guid + ', address: ' + user_4_address)
 TEST FOR CHECKING BALANCE
 """
 # Check balance
-payload_balance_1 = {'password': payload_user_1.get('password'), 'api_code': payload_user_1.get('api_code')}
+payload_balance_1 = {'password': payload_user_1.get('password'),
+                     'api_code': payload_user_1.get('api_code')}
+payload_balance_2 = {'password': payload_user_2.get('password'),
+                     'api_code': payload_user_2.get('api_code')}
+payload_balance_3 = {'password': payload_user_3.get('password'),
+                     'api_code': payload_user_3.get('api_code')}
+payload_balance_4 = {'password': payload_user_4.get('password'),
+                     'api_code': payload_user_4.get('api_code')}
 try:
-    account_balance_1 = requests.get('http://localhost:3000/merchant/' + user_1_guid + '/balance', params=payload_balance_1)
+    account_balance_1 = requests.get('http://localhost:3000/merchant/' + user_1_guid + '/balance',
+                                     params=payload_balance_1)
 except requests.exceptions.SSLError:
-    print("Connection refused!")
-print('[BALANCE] ' + account_balance_1.json().get('balance'))
-print(account_balance_1.status_code)
+    print("Connection refused! Status code: " + account_balance_1.status_code)
+try:
+    account_balance_2 = requests.get('http://localhost:3000/merchant/' + user_2_guid + '/balance',
+                                     params=payload_balance_2)
+except requests.exceptions.SSLError:
+    print("Connection refused! Status code: " + account_balance_2.status_code)
+try:
+    account_balance_3 = requests.get('http://localhost:3000/merchant/' + user_3_guid + '/balance',
+                                     params=payload_balance_3)
+except requests.exceptions.SSLError:
+    print("Connection refused! Status code: " + account_balance_3.status_code)
+try:
+    account_balance_4 = requests.get('http://localhost:3000/merchant/' + user_4_guid + '/balance',
+                                     params=payload_balance_4)
+except requests.exceptions.SSLError:
+    print("Connection refused! Status code: " + account_balance_4.status_code)
+print('[BALANCE_1] ' + str(account_balance_1.json().get('balance')))
+print('[BALANCE_2] ' + str(account_balance_2.json().get('balance')))
+print('[BALANCE_3] ' + str(account_balance_3.json().get('balance')))
+print('[BALANCE_4] ' + str(account_balance_4.json().get('balance')))
 
 
 """
 TEST FOR MAKING OUTGOING PAYMENTS
+http://localhost:3000/merchant/$guid/payment?password=$main_password&second_password=$second_password&to=$address&amount=$amount&from=$from&fee=$fee
+$main_password Your Main Blockchain Wallet password
+$second_password Your second Blockchain Wallet password if double encryption is enabled.
+$to Recipient Bitcoin Address.
+$amount Amount to send in satoshi.
+$from Send from a specific Bitcoin Address (Optional)
+$fee Transaction fee value in satoshi (Must be greater than default fee) (Optional)
 """
+payload_payment_1_2 = {'password': payload_user_1.get('password'),
+                       'to': user_2_address,
+                       'amount': 5000,
+                       'fee': 10}
 
+try:
+    payment_1_2 = requests.get('http://localhost:3000/merchant/' + user_1_guid + '/payment',
+                               params=payload_payment_1_2)
+except requests.exceptions.HTTPError:
+    print("Connection refused! Status code: " + payment_1_2.status_code)
+
+print(payment_1_2.status_code)
+print(payment_1_2.json().get('message'), payment_1_2.json().get('tx_hash'), payment_1_2.json().get('notice'))
 # print(payload_payment_1.get('message'))
 # print(payment_user_1.text)
 # print(payment_user_1.get('tx_hash'))
 # print(payment_user_1.get('notice'))
 # print('Status: ' + str(payment_user_1.status_code) + ', message: ' + payment_user_1.json())
-
-
-#
 
